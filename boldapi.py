c6d0d1e5ef61e8d62d6fccc1624249d20ad1a3f5
kiralong <kiralong778@gmail.com>
2026-03-18 16:39:00 -0400

    Added BIN list and for loop start

--- boldapi.py
+++ boldapi.py
@@ -55,37 +55,97 @@ HEADERS = {'accept': 'application/json'}
 # > Processes and resolves query tokens if matches are found. If matches are not found, will attempt 
 # > to infer possible values for the intended search and return successfully. If triplet token cannot 
 # > be resolved unambiguously, will return possible values but return as an error.
-response = requests.get(f"{API_BASE_URL}/query/preprocessor", headers=HEADERS, params={"query": f"bin:{TARGET_BIN}"})
-assert response.status_code == 200
+# response = requests.get(f"{API_BASE_URL}/query/preprocessor", headers=HEADERS, params={"query": f"bin:na:{TARGET_BIN}"})
+# assert response.status_code == 200
 # We get back: A preprpocessed query to be used in the actual query step
 # Response:
 # {
 #   "successful_terms": [
 #     {
-#       "submitted": "bin:BOLD:ACQ9269",
+#       "submitted": "bin:uri:BOLD:ACQ9269",
 #       "matched": "ids:processid:ACQ9269;ids:sampleid:ACQ9269;ids:insdcacs:ACQ9269"
 #     }
 #   ]
 # }
-queries = [successful_term.get('matched') for successful_term in response.json()["successful_terms"]]
+# queries = [successful_term.get('matched') for successful_term in response.json()["successful_terms"]]
 
 
-# STEP 2: Now that we have a preprocessed query, we make the actual query.
-# TODO Add a foreach here, but for now just do the first
-query = queries[0]
-response = requests.get(f"{API_BASE_URL}/query", headers=HEADERS, params={"extent": "limited", "query": query})
-assert response.status_code == 200
-# Response: {
-#   "query_id": "eAHLTCm2yswrTklOTC62cnQOtDQys7TOBAoWFOUnpxYXZ6agiBYn5hbkpCIJ5mTmZpakpgAAbVsYJg==",
-#   "extent_limit": 1000
-# }
-query_id = response.json()["query_id"]
+# # STEP 1: We need to get the query id (which we already know the format as bin:uri:TARGET_BIN from parse and preprocessor)
+# # TODO Add a foreach here, but for now just do the first
+# # query = queries[0]
+# query = f"bin:uri:{TARGET_BIN}"
+# response = requests.get(f"{API_BASE_URL}/query", headers=HEADERS, params={"extent": "limited", "query": query})
+# assert response.status_code == 200
+# # Response: {
+# #   "query_id": "eAHLTCm2yswrTklOTC62cnQOtDQys7TOBAoWFOUnpxYXZ6agiBYn5hbkpCIJ5mTmZpakpgAAbVsYJg==",
+# #   "extent_limit": 1000
+# # }
+# query_id = response.json()["query_id"]
 
-# https://portal.boldsystems.org/api/documents/<query_id>/download?format=json
+# # https://portal.boldsystems.org/api/documents/<query_id>/download?format=json
 
-response = requests.get(f"{API_BASE_URL}/documents/{query_id}/download", headers=HEADERS, params={"format": "json"})
-assert response.status_code == 200
+# response = requests.get(f"{API_BASE_URL}/documents/{query_id}/download", headers=HEADERS, params={"format": "json"})
+# #assert response.status_code == 200
+
+# response = requests.get(f"{API_BASE_URL}/taxonomy/{query_id}", headers=HEADERS, params={"format": "json"})
+# # To see what response has
+# print(response.text)
+
+# # Pull out the genus information
+# # Save the taxonomy summary into a data variable
+# taxa_data = response.json()
+
+# # See the dictionary keys
+# list(taxa_data.keys())
+
+# # Returns ['taxonomy']
+# list(taxa_data['taxonomy'].keys())
+# # Returns ['kingdom', 'phylum', 'class', 'order', 'family', 'subfamily', 'tribe', 'genus', 'species', 'subspecies']
+
+# # See what is in the dictionary genus
+# print(taxa_data['taxonomy']["genus"])
+
+# # Save just the key from genus into variable genus and make it a string
+# genus = str(list(taxa_data['taxonomy']["genus"].keys()))
+
+# Now to make a loop to read in list of BINs, pull the taxonomy info, and populate a final list with the genus
+file_path = 'BIN_list.tsv'
+
+count = 1
+try:
+    with open(file_path, 'r') as file:
+        for line in file:
+            # The 'line' variable contains the current line, including the newline character '\n'
+            # Use line.strip() to remove leading/trailing whitespace and the newline
+            processed_line = line.strip()
+            
+            # Perform operations on the processed line (e.g., print it, store it, etc.)
+            # check processed line
+            # print(processed_line)
+
+            TARGET_BIN = processed_line
+            print(TARGET_BIN)
+            query = f"bin:uri:{TARGET_BIN}"
+            response = requests.get(f"{API_BASE_URL}/query", headers=HEADERS, params={"extent": "limited", "query": query})
+            query_id = response.json()["query_id"]
+
+            # Check query id is as expected
+            # print(query_id)
+
+            # Get taxonomy for the target bin using the query_id
+            response = requests.get(f"{API_BASE_URL}/taxonomy/{query_id}", headers=HEADERS, params={"format": "json"})
+            print(response.status_code)
+            print(response.text)
+            # Save the taxonomy summary into a data variable
+            taxa_data = response.json()
+            genus = str(list(taxa_data['taxonomy']["genus"].keys()))
+            print(f"{count}:{genus}")
+            count += 1
+            #print(count) d
+            #print(genus)
 
 
+except FileNotFoundError:
+    print(f"Error: The file '{file_path}' was not found.")
 
-
+# NOTE this is failing at DIFFERENT lines each time...
